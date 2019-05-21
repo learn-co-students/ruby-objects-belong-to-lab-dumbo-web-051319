9dcfbdd95df900c2c93d2f119f8f3ae479cd9436
Kenton Archer <kenton8654@yahoo.com>
2019-05-20 21:43:45 -0400

    Done.

---
+++ lib/post.rb
@@ -0,0 +1,20 @@
+# require 'lib'
+
+class Post
+
+  attr_accessor :title,:author
+
+ #  def initialize(title)
+ #    @title = title
+ #  end
+ #
+ # yagga = Post.new("Yagga")
+ # yagga.author = kenton_1
+
+
+
+
+
+
+
+end
---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+# require 'lib'
+class Song
+
+attr_accessor :title,:artist
+#
+# def initialize(title)
+#   @title = title
+# end
+#
+# get_up = Song.new("Get up")
+# get_up.artist = j_cole
+#
+end
